e00e3e73d806eb0df7bd26978b8957f3c0cbc627
Benjamin-Cardon <benjamin.p.cardon@gmail.com>
2024-07-13 19:20:59 -0600

    Added get route for portal information

--- src/routes/librarian/portal.ts
+++ src/routes/librarian/portal.ts
@@ -1,7 +1,10 @@
 const express = require('express')
-const portal = express.Router()
+const portal: Router = express.Router()
+import { Router, Request, Response } from "express";
 import client from "../../db/client";
 
+portal.get('/student_info', async (req: Request, res: Response) => {
 
+})
 
 export default portal;
